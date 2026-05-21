e5231c4698fcb659f82668096aa83d6b3ed5fc3c
akellofatumakedrine-create <akellofatumakedrine@gmail.com>
2026-05-21 18:03:16 +0300

    week03 work

--- week03/code/FeatureCollection.cs
+++ week03/code/FeatureCollection.cs
@@ -1,5 +1,21 @@
-public class FeatureCollection
+using System.Collections.Generic;
+using System.Text.Json;
+
+namespace Week03
 {
-    // TODO Problem 5 - ADD YOUR CODE HERE
-    // Create additional classes as necessary
+    public class FeatureCollection
+    {
+        public List<Feature> features { get; set; }
+    }
+
+    public class Feature
+    {
+        public Properties properties { get; set; }
+    }
+
+    public class Properties
+    {
+        public string place { get; set; }
+        public double mag { get; set; }
+    }
 }

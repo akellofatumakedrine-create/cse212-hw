0d107a8fc7180be970040462d9801cffdf2a4b6b
akellofatumakedrine-create <akellofatumakedrine@gmail.com>
2026-06-11 16:42:08 +0300

    week06 adjustments

--- week06/code/BinarySearchTree.cs
+++ week06/code/BinarySearchTree.cs
@@ -4,43 +4,39 @@ public class BinarySearchTree : IEnumerable<int>
 {
     private Node? _root;
 
+
     /// <summary>
     /// Insert a new node in the BST.
     /// </summary>
     public void Insert(int value)
     {
-        // Create new node
         Node newNode = new(value);
-        // If the list is empty, then point both head and tail to the new node.
+
         if (_root is null)
         {
             _root = newNode;
         }
-        // If the list is not empty, then only head will be affected.
         else
         {
             _root.Insert(value);
         }
     }
 
+
     /// <summary>
     /// Check to see if the tree contains a certain value
     /// </summary>
-    /// <param name="value">The value to look for</param>
-    /// <returns>true if found, otherwise false</returns>
     public bool Contains(int value)
     {
         return _root != null && _root.Contains(value);
     }
 
-    /// <summary>
-    /// Yields all values in the tree
-    /// </summary>
+
     IEnumerator IEnumerable.GetEnumerator()
     {
-        // call the generic version of the method
         return GetEnumerator();
     }
+
 
     /// <summary>
     /// Iterate forward through the BST
@@ -48,50 +44,73 @@ public class BinarySearchTree : IEnumerable<int>
     public IEnumerator<int> GetEnumerator()
     {
         var numbers = new List<int>();
+
         TraverseForward(_root, numbers);
+
         foreach (var number in numbers)
         {
             yield return number;
         }
     }
 
+
     private void TraverseForward(Node? node, List<int> values)
     {
-        if (node is not null)
+        if (node != null)
         {
             TraverseForward(node.Left, values);
+
             values.Add(node.Data);
+
             TraverseForward(node.Right, values);
         }
     }
 
+
+
     /// <summary>
-    /// Iterate backward through the BST.
+    /// Iterate backward through the BST
     /// </summary>
-    public IEnumerable Reverse()
+    public IEnumerable<int> Reverse()
     {
         var numbers = new List<int>();
+
         TraverseBackward(_root, numbers);
+
         foreach (var number in numbers)
         {
             yield return number;
         }
     }
 
+
     private void TraverseBackward(Node? node, List<int> values)
     {
-        // TODO Problem 3
+        if (node != null)
+        {
+            // Right -> Node -> Left
+            TraverseBackward(node.Right, values);
+
+            values.Add(node.Data);
+
+            TraverseBackward(node.Left, values);
+        }
     }
+
+
 
     /// <summary>
     /// Get the height of the tree
     /// </summary>
     public int GetHeight()
     {
-        if (_root is null)
+        if (_root == null)
             return 0;
+
         return _root.GetHeight();
     }
+
+
 
     public override string ToString()
     {
@@ -99,8 +118,14 @@ public class BinarySearchTree : IEnumerable<int>
     }
 }
 
-public static class IntArrayExtensionMethods {
-    public static string AsString(this IEnumerable array) {
-        return "<IEnumerable>{" + string.Join(", ", array.Cast<int>()) + "}";
+
+
+public static class IntArrayExtensionMethods
+{
+    public static string AsString(this IEnumerable array)
+    {
+        return "<IEnumerable>{" +
+            string.Join(", ", array.Cast<int>()) +
+            "}";
     }
 }
--- week06/code/Node.cs
+++ week06/code/Node.cs
@@ -1,45 +1,88 @@
 public class Node
 {
     public int Data { get; set; }
+
     public Node? Right { get; private set; }
+
     public Node? Left { get; private set; }
+
 
     public Node(int data)
     {
-        this.Data = data;
+        Data = data;
     }
 
+
+
+    // Problem 1: Insert Unique Values Only
     public void Insert(int value)
     {
-        // TODO Start Problem 1
+        // duplicate value
+        if (value == Data)
+        {
+            return;
+        }
+
 
         if (value < Data)
         {
-            // Insert to the left
-            if (Left is null)
+            if (Left == null)
+            {
                 Left = new Node(value);
+            }
             else
+            {
                 Left.Insert(value);
+            }
         }
         else
         {
-            // Insert to the right
-            if (Right is null)
+            if (Right == null)
+            {
                 Right = new Node(value);
+            }
             else
+            {
                 Right.Insert(value);
+            }
         }
     }
 
+
+
+    // Problem 2: Contains
     public bool Contains(int value)
     {
-        // TODO Start Problem 2
-        return false;
+        if (value == Data)
+        {
+            return true;
+        }
+
+
+        if (value < Data)
+        {
+            return Left != null && Left.Contains(value);
+        }
+
+
+        return Right != null && Right.Contains(value);
     }
 
+
+
+    // Problem 4: Tree Height
     public int GetHeight()
     {
-        // TODO Start Problem 4
-        return 0; // Replace this line with the correct return statement(s)
+        int leftHeight = Left == null 
+            ? 0 
+            : Left.GetHeight();
+
+
+        int rightHeight = Right == null 
+            ? 0 
+            : Right.GetHeight();
+
+
+        return 1 + Math.Max(leftHeight, rightHeight);
     }
 }
